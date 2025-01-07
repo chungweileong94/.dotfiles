2b10c2f0ccb15ee387110a038b115fbd8dd94d8a
Chung Wei <chungwei@whiteroom.work>
2025-01-07 16:36:29 +0800

    commit ghostty config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,11 @@
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 # zed config
+mkdir -p ~/.config/zed
 ln -s ~/.dotfiles/.config/zed/settings.json ~/.config/zed/settings.json
 ln -s ~/.dotfiles/.config/zed/keymap.json ~/.config/zed/keymap.json
+
+# ghostty config
+mkdir -p ~/.config/ghostty
+ln -s ~/.dotfiles/.config/ghostty/config ~/.config/ghostty/config
+ln -s ~/.dotfiles/.config/ghostty/themes ~/.config/ghostty/themes
