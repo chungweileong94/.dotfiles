4f7a1bcc64aa7012da6e1b343909bd40d06e2129
Chung Wei <chungwei@whiteroom.work>
2025-01-07 16:18:27 +0800

    commit zed config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,6 @@
+# .zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
+# zed config
+ln -s ~/.dotfiles/.config/zed/settings.json ~/.config/zed/settings.json
+ln -s ~/.dotfiles/.config/zed/keymap.json ~/.config/zed/keymap.json
